59a3c755fe8582465a6dbf1d7df68592ba1b51cd
zz358840 <kevin@kingkit101.com>
2021-10-01 11:50:55 +0800

    feat: update compilerModel.sh

--- compilerModel.sh
+++ compilerModel.sh
@@ -1,4 +1,6 @@
 rm -rf out
+mkdir -p /content/drive/MyDrive/datasets
+mv /content/drive/MyDrive/$1 /content/drive/MyDrive/datasets
 python3 cvtTMFile.py /content/drive/MyDrive/datasets/$1
 python3 train.py init
 python3 train.py -t classifier -d /content/drive/MyDrive/datasets/$1.datasets train
