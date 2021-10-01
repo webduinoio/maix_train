1858545c5aed5232485b40b9a4216a8a65e3cd6d
zz358840 <kevin@kingkit101.com>
2021-10-01 11:48:08 +0800

    feat: update config

--- train/config_template.py
+++ train/config_template.py
@@ -8,7 +8,7 @@ curr_dir = os.path.abspath(os.path.dirname(__file__))
 # kmodel convert
                  # "/ncc/ncc"  # download from https://github.com/kendryte/nncase/releases/tag/v0.1.0-rc5
 ncc_kmodel_v3 =  os.path.join(curr_dir, "..", "tools", "ncc", "ncc_v0.1/ncc")  
-sample_image_num = 100       # convert kmodel sample image (for quantizing)
+sample_image_num = 20       # convert kmodel sample image (for quantizing)
 
 # train
 allow_cpu = True # True
@@ -16,9 +16,9 @@ allow_cpu = True # True
 # classifier
 classifier_train_gpu_mem_require = 2*1024*1024*1024
 classifier_train_epochs = 30
-classifier_train_batch_size = 64
+classifier_train_batch_size = 10
 classifier_train_max_classes_num = 15
-classifier_train_one_class_min_img_num = 10            # 一个类别中至少需要的样本数量
+classifier_train_one_class_min_img_num = 40            # 一个类别中至少需要的样本数量
 classifier_train_one_class_max_img_num = 2000          # 一个类别中最多需要的样本数量
 classifier_result_file_name_prefix = "maixhub_classifier_result"
 
